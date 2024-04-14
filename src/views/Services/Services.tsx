a517598944e669a624dd7fb9dfd6adbae76dbe2c
ClaraLee95 <guswl761@gmail.com>
2024-04-14 15:05:56 +0200

    Was Biete Ich page completed

--- src/views/Services/Services.tsx
+++ src/views/Services/Services.tsx
@@ -1,18 +1,36 @@
 import React from "react";
-import image from "../../assets/background.jpg";
+import image from "../../assets/services_example.png";
 
 import "./Services.scss";
 
 function Services() {
 	return (
-		<div>
-			<h1>Was Biete Ich</h1>
-			<img
-				className=""
-				src={image}
-				alt=""
-				style={{ width: 1200, margin: "auto" }}
-			/>
+		<div className="servicesContainer">
+			<div className="servicesTop">
+				<div className="servicesTitle">Was Biete Ich</div>
+				<div className="servicesSubtitle">
+					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+					nisi ut aliquip ex.
+				</div>
+			</div>
+			<div className="servicesImage">
+				<img src={image} alt="Was Biete Ich" />
+			</div>
+			<div className="servicesText fontText">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur.
+				<br />
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+				veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+				commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+				velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit
+				amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua.
+			</div>
 		</div>
 	);
 }
