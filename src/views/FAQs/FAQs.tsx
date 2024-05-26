acce423bceb7d661d77c9535adb07ad42e854591
ClaraLee95 <guswl761@gmail.com>
2024-05-26 21:07:56 +0200

    FAQs page completed

--- src/views/FAQs/FAQs.tsx
+++ src/views/FAQs/FAQs.tsx
@@ -1,18 +1,47 @@
 import React from "react";
-import image from "../../assets/background.jpg";
+import Accordion from "react-bootstrap/Accordion";
 
 import "./FAQs.scss";
 
 function FAQs() {
 	return (
-		<div>
-			<h1>FAQs</h1>
-			<img
-				className=""
-				src={image}
-				alt=""
-				style={{ width: 1200, margin: "auto" }}
-			/>
+		<div className="faqContainer">
+			<div className="faqHeaderContainer">
+				<div className="faqHeader fontHeaderBig fontLight">FAQs</div>
+			</div>
+			<div className="faqContentContainer fontText">
+				<Accordion alwaysOpen>
+					<Accordion.Item eventKey="0">
+						<Accordion.Header>Accordion Item #1</Accordion.Header>
+						<Accordion.Body>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore eu fugiat
+							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</Accordion.Body>
+					</Accordion.Item>
+					<Accordion.Item eventKey="1">
+						<Accordion.Header>Accordion Item #2</Accordion.Header>
+						<Accordion.Body>
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore eu fugiat
+							nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+							sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</Accordion.Body>
+					</Accordion.Item>
+					<Accordion.Item eventKey="2">
+						<Accordion.Header>Accordion Item #2</Accordion.Header>
+						<Accordion.Body>
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+							officia deserunt mollit anim id est laborum.
+						</Accordion.Body>
+					</Accordion.Item>
+				</Accordion>
+			</div>
 		</div>
 	);
 }
