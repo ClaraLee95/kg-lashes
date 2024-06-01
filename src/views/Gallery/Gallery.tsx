c2ae02177d8cdacea0abcf50e1ad0ab7b14da5e3
ClaraLee95 <guswl761@gmail.com>
2024-06-02 01:11:30 +0200

    Gallery updated

--- src/views/Gallery/Gallery.tsx
+++ src/views/Gallery/Gallery.tsx
@@ -1,18 +1,78 @@
 import React from "react";
-import image from "../../assets/background.jpg";
+import example from "../../assets/home_gallery_example1.jpg";
 
 import "./Gallery.scss";
 
 function Gallery() {
 	return (
-		<div>
-			<h1>Gallery</h1>
-			<img
-				className=""
-				src={image}
-				alt=""
-				style={{ width: 1200, margin: "auto" }}
-			/>
+		<div className="galleryContainer">
+			<div className="galleryTop">
+				<div className="galleryTitle fontHeaderBig">Galerie</div>
+			</div>
+			<div className="galleryContentContainer">
+				<div className="galleryLeft">
+					<div className="galleryBig">
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+					</div>
+					<div className="gallerySmall">
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+					</div>
+				</div>
+				<div className="galleryRight">
+					<div className="gallerySmall">
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+					</div>
+					<div className="galleryBig">
+						<img
+							src={example}
+							alt="Gallery Example"
+							loading="lazy"
+							decoding="async"></img>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
